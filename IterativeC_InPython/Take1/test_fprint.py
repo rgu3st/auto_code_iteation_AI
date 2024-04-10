8b6e40f70d80a90a40d130302a34c20699d487f6
rgu3st <robguest@gmail.com>
2024-04-10 14:17:46 -0500

    First commit. Works well enough with OpenAI, gpt3.5-turbo.

---
+++ IterativeC_InPython/Take1/test_fprint.py
@@ -0,0 +1,5 @@
+here = "Python."
+str1 = f"Hello World, from {here}!\n"
+
+here = "farther down."
+print(str1)
---
+++ IterativeC_InPython/c_code.c
@@ -0,0 +1,10 @@
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Hi, iteratively fixing AI!\n");
+
+    return 0; // Fixing the issue by directly returning 0
+}
+
